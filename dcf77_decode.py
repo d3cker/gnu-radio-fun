e3275e23cef4376678ca078d5347cf787a32d307
Bartłomiej Marcinkowski <bartlomiej.marcinkowski@contractors.roche.com>
2020-07-11 17:17:42 +0200

    added dc77 base code

--- dcf77_decode.py
+++ dcf77_decode.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 ##################################################
 # GNU Radio Python Flow Graph
-# Title: Am Receiver
-# Generated: Sat Jul 11 09:44:15 2020
+# Title: Dcf77 Decode
+# Generated: Sat Jul 11 17:15:34 2020
 ##################################################
 
 
@@ -17,6 +17,7 @@ if __name__ == '__main__':
         except:
             print "Warning: failed to XInitThreads()"
 
+from gnuradio import analog
 from gnuradio import audio
 from gnuradio import blocks
 from gnuradio import eng_notation
@@ -30,15 +31,16 @@ from gnuradio.wxgui import fftsink2
 from gnuradio.wxgui import forms
 from grc_gnuradio import wxgui as grc_wxgui
 from optparse import OptionParser
+import math
 import osmosdr
 import time
 import wx
 
 
-class am_receiver(grc_wxgui.top_block_gui):
+class dcf77_decode(grc_wxgui.top_block_gui):
 
     def __init__(self):
-        grc_wxgui.top_block_gui.__init__(self, title="Am Receiver")
+        grc_wxgui.top_block_gui.__init__(self, title="Dcf77 Decode")
         _icon_path = "/usr/share/icons/hicolor/32x32/apps/gnuradio-grc.png"
         self.SetIcon(wx.Icon(_icon_path, wx.BITMAP_TYPE_ANY))
 
@@ -47,7 +49,7 @@ class am_receiver(grc_wxgui.top_block_gui):
         ##################################################
         self.volume = volume = 1
         self.sound_frequency = sound_frequency = 800
-        self.samp_rate = samp_rate = 768000
+        self.samp_rate = samp_rate = 192000
         self.narrow_filter = narrow_filter = 200
         self.freq_adjust = freq_adjust = 77500
         self.decimation = decimation = 1
@@ -148,22 +150,6 @@ class am_receiver(grc_wxgui.top_block_gui):
         	proportion=1,
         )
         self.Add(_freq_adjust_sizer)
-        self.wxgui_fftsink2_1 = fftsink2.fft_sink_c(
-        	self.GetWin(),
-        	baseband_freq=freq_adjust,
-        	y_per_div=10,
-        	y_divs=10,
-        	ref_level=0,
-        	ref_scale=2.0,
-        	sample_rate=samp_rate,
-        	fft_size=1024,
-        	fft_rate=15,
-        	average=False,
-        	avg_alpha=None,
-        	title='FFT Plot',
-        	peak_hold=False,
-        )
-        self.Add(self.wxgui_fftsink2_1.win)
         self.wxgui_fftsink2_0_0 = fftsink2.fft_sink_f(
         	self.GetWin(),
         	baseband_freq=0,
@@ -194,7 +180,6 @@ class am_receiver(grc_wxgui.top_block_gui):
         	avg_alpha=None,
         	title='Radio spectrum',
         	peak_hold=False,
-        	win=window.blackmanharris,
         )
         self.Add(self.wxgui_fftsink2_0.win)
         self.rtlsdr_source_0 = osmosdr.source( args="numchan=" + str(1) + " " + '' )
@@ -218,25 +203,29 @@ class am_receiver(grc_wxgui.top_block_gui):
                 taps=None,
                 fractional_bw=None,
         )
+        self.low_pass_filter_0 = filter.fir_filter_ccf(1, firdes.low_pass(
+        	1, samp_rate, freq_adjust+1000, 1000, firdes.WIN_HAMMING, 6.76))
         self.freq_xlating_fir_filter_xxx_0 = filter.freq_xlating_fir_filter_ccf(1, (firdes.low_pass(1,samp_rate,samp_rate/(2*decimation), sound_frequency)), sound_frequency, samp_rate)
         self.blocks_multiply_const_vxx_0 = blocks.multiply_const_vff((volume, ))
         self.blocks_complex_to_real_0 = blocks.complex_to_real(1)
         self.band_pass_filter_0 = filter.fir_filter_ccf(1, firdes.band_pass(
-        	1, samp_rate, sound_frequency-narrow_filter, sound_frequency+narrow_filter, sound_frequency, firdes.WIN_HAMMING, 6.76))
+        	3, samp_rate, sound_frequency-narrow_filter, sound_frequency+narrow_filter, sound_frequency, firdes.WIN_HAMMING, 6.76))
         self.audio_sink_0 = audio.sink(audio_rate, '', True)
+        self.analog_quadrature_demod_cf_0 = analog.quadrature_demod_cf(samp_rate/(2*math.pi*120000/8.0))
 
         ##################################################
         # Connections
         ##################################################
+        self.connect((self.analog_quadrature_demod_cf_0, 0), (self.wxgui_fftsink2_0_0, 0))
+        self.connect((self.band_pass_filter_0, 0), (self.analog_quadrature_demod_cf_0, 0))
         self.connect((self.band_pass_filter_0, 0), (self.blocks_complex_to_real_0, 0))
         self.connect((self.blocks_complex_to_real_0, 0), (self.rational_resampler_xxx_0, 0))
         self.connect((self.blocks_multiply_const_vxx_0, 0), (self.audio_sink_0, 0))
-        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.wxgui_fftsink2_0_0, 0))
         self.connect((self.freq_xlating_fir_filter_xxx_0, 0), (self.band_pass_filter_0, 0))
-        self.connect((self.freq_xlating_fir_filter_xxx_0, 0), (self.wxgui_fftsink2_1, 0))
+        self.connect((self.freq_xlating_fir_filter_xxx_0, 0), (self.low_pass_filter_0, 0))
+        self.connect((self.low_pass_filter_0, 0), (self.wxgui_fftsink2_0, 0))
         self.connect((self.rational_resampler_xxx_0, 0), (self.blocks_multiply_const_vxx_0, 0))
         self.connect((self.rtlsdr_source_0, 0), (self.freq_xlating_fir_filter_xxx_0, 0))
-        self.connect((self.rtlsdr_source_0, 0), (self.wxgui_fftsink2_0, 0))
 
     def get_volume(self):
         return self.volume
@@ -256,18 +245,19 @@ class am_receiver(grc_wxgui.top_block_gui):
         self._sound_frequency_text_box.set_value(self.sound_frequency)
         self.freq_xlating_fir_filter_xxx_0.set_taps((firdes.low_pass(1,self.samp_rate,self.samp_rate/(2*self.decimation), self.sound_frequency)))
         self.freq_xlating_fir_filter_xxx_0.set_center_freq(self.sound_frequency)
-        self.band_pass_filter_0.set_taps(firdes.band_pass(1, self.samp_rate, self.sound_frequency-self.narrow_filter, self.sound_frequency+self.narrow_filter, self.sound_frequency, firdes.WIN_HAMMING, 6.76))
+        self.band_pass_filter_0.set_taps(firdes.band_pass(3, self.samp_rate, self.sound_frequency-self.narrow_filter, self.sound_frequency+self.narrow_filter, self.sound_frequency, firdes.WIN_HAMMING, 6.76))
 
     def get_samp_rate(self):
         return self.samp_rate
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.wxgui_fftsink2_1.set_sample_rate(self.samp_rate)
         self.wxgui_fftsink2_0.set_sample_rate(self.samp_rate)
         self.rtlsdr_source_0.set_sample_rate(self.samp_rate)
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, self.freq_adjust+1000, 1000, firdes.WIN_HAMMING, 6.76))
         self.freq_xlating_fir_filter_xxx_0.set_taps((firdes.low_pass(1,self.samp_rate,self.samp_rate/(2*self.decimation), self.sound_frequency)))
-        self.band_pass_filter_0.set_taps(firdes.band_pass(1, self.samp_rate, self.sound_frequency-self.narrow_filter, self.sound_frequency+self.narrow_filter, self.sound_frequency, firdes.WIN_HAMMING, 6.76))
+        self.band_pass_filter_0.set_taps(firdes.band_pass(3, self.samp_rate, self.sound_frequency-self.narrow_filter, self.sound_frequency+self.narrow_filter, self.sound_frequency, firdes.WIN_HAMMING, 6.76))
+        self.analog_quadrature_demod_cf_0.set_gain(self.samp_rate/(2*math.pi*120000/8.0))
 
     def get_narrow_filter(self):
         return self.narrow_filter
@@ -276,7 +266,7 @@ class am_receiver(grc_wxgui.top_block_gui):
         self.narrow_filter = narrow_filter
         self._narrow_filter_slider.set_value(self.narrow_filter)
         self._narrow_filter_text_box.set_value(self.narrow_filter)
-        self.band_pass_filter_0.set_taps(firdes.band_pass(1, self.samp_rate, self.sound_frequency-self.narrow_filter, self.sound_frequency+self.narrow_filter, self.sound_frequency, firdes.WIN_HAMMING, 6.76))
+        self.band_pass_filter_0.set_taps(firdes.band_pass(3, self.samp_rate, self.sound_frequency-self.narrow_filter, self.sound_frequency+self.narrow_filter, self.sound_frequency, firdes.WIN_HAMMING, 6.76))
 
     def get_freq_adjust(self):
         return self.freq_adjust
@@ -285,9 +275,9 @@ class am_receiver(grc_wxgui.top_block_gui):
         self.freq_adjust = freq_adjust
         self._freq_adjust_slider.set_value(self.freq_adjust)
         self._freq_adjust_text_box.set_value(self.freq_adjust)
-        self.wxgui_fftsink2_1.set_baseband_freq(self.freq_adjust)
         self.wxgui_fftsink2_0.set_baseband_freq(self.freq_adjust)
         self.rtlsdr_source_0.set_center_freq(self.freq_adjust, 0)
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, self.freq_adjust+1000, 1000, firdes.WIN_HAMMING, 6.76))
 
     def get_decimation(self):
         return self.decimation
@@ -304,7 +294,7 @@ class am_receiver(grc_wxgui.top_block_gui):
         self.wxgui_fftsink2_0_0.set_sample_rate(self.audio_rate)
 
 
-def main(top_block_cls=am_receiver, options=None):
+def main(top_block_cls=dcf77_decode, options=None):
 
     tb = top_block_cls()
     tb.Start(True)
